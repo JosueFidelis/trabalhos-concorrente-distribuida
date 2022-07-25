edf6ade93b9db2d324e675f16fb3d5b53a5cc6ee
Luana Porciuncula <lpb@cin.ufpe.br>
2022-07-25 01:40:53 -0300

    adicionadas threads de 2, 5 e 10 da atividade 1

--- exercicio_1/file_reader_tester.go
+++ exercicio_1/file_reader_tester.go
@@ -8,6 +8,8 @@ import (
 	"time"
 )
 
+var n_lorem = 100
+
 func logDuration(fileName string, start time.Time) {
 	elapsed := time.Since(start).Microseconds()
 
@@ -21,7 +23,7 @@ func logDuration(fileName string, start time.Time) {
 	log.Printf("%d\n", elapsed)
 }
 
-func readLoremFile(beginning int, end int) {
+func readLoremFile(beginning int, end int, active_thread_pointer *chan int) {
 	baseFile := "lorem"
 	var file strings.Builder
 
@@ -38,27 +40,66 @@ func readLoremFile(beginning int, end int) {
 		file.Reset()
 	}
 
+	*active_thread_pointer <- 1
 }
 
 //O professor falou que o início do programa pode enfrentar instabilidade
 func iterationsToDiscard() {
+	//Cria um canal e preenche ele, só para o readLoremFile conseguir remover algo do canal ao final
+	var active_thread_chan = make(chan int, 1)
+
 	for i := 0; i < 100; i++ {
-		readLoremFile(1, 100)
+		readLoremFile(1, n_lorem, &active_thread_chan)
+		<-active_thread_chan
 	}
 }
 
-func singleThreadTest() {
-	start := time.Now()
-	defer logDuration("single_thread_time_elapsed.txt", start)
+func createLoremThreadTest(n_threads int) {
+	var out_file_name strings.Builder
 
-	readLoremFile(1, 100)
+	out_file_name.WriteString(fmt.Sprint(n_threads))
+	out_file_name.WriteString("_thread_elapsed_time.txt")
+
+	start := time.Now()
+	defer logDuration(out_file_name.String(), start)
+
+	// Monitora threads ativas para que a função createLoremThreadTest espere que todas as threads terminem
+	var active_thread_chan = make(chan int, n_threads)
+
+	s_range := 1
+	for i := 0; i < n_threads; i++ {
+		e_range := (s_range - 1) + n_lorem/n_threads
+		go readLoremFile(s_range, e_range, &active_thread_chan)
+		s_range += n_lorem / n_threads
+	}
+
+	// Tenta esvaziar o canal para garantir que as threads terminaram
+	for i := 0; i < n_threads; i++ {
+		<-active_thread_chan
+	}
 }
 
 func main() {
 	iterationsToDiscard()
 
 	for i := 0; i < 30; i++ {
-		singleThreadTest()
+		// Single Thread
+		createLoremThreadTest(1)
+	}
+
+	for i := 0; i < 30; i++ {
+		// Two Threads
+		createLoremThreadTest(2)
+	}
+
+	for i := 0; i < 30; i++ {
+		// Five Threads
+		createLoremThreadTest(5)
+	}
+
+	for i := 0; i < 30; i++ {
+		// Ten Threads
+		createLoremThreadTest(10)
 	}
 }
 
