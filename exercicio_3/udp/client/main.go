dd702830a7fb017896acc44abdd4a2c90911104f
Luana Porciuncula <lpb@cin.ufpe.br>
2022-08-20 00:44:04 -0300

    create simple UDP Client-Server

--- exercicio_3/udp/client/main.go
+++ exercicio_3/udp/client/main.go
@@ -1,5 +1,44 @@
-package client
+package main
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+)
+
+func logErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
 
 func main() {
+	rep := make([]byte, 1024)
+	n := 30
 
+	addr, err := net.ResolveUDPAddr("udp", "localhost:8081")
+	logErr(err)
+
+	conn, err := net.DialUDP("udp", nil, addr)
+	logErr(err)
+
+	defer conn.Close()
+
+	for i := 0; i < n; i++ {
+		go sendMsg(*conn, i)
+
+		_, _, err = conn.ReadFromUDP(rep)
+		logErr(err)
+		fmt.Println(string(rep[:]))
+	}
+}
+
+func sendMsg(conn net.UDPConn, i int) {
+	req := make([]byte, 1024)
+
+	req = []byte("Mensagem " + strconv.Itoa(i+1))
+
+	_, err := conn.Write(req)
+	logErr(err)
 }
--- exercicio_3/udp/server/main.go
+++ exercicio_3/udp/server/main.go
@@ -1,5 +1,50 @@
-package server
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+func logErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
 
 func main() {
 
+	addr, err := net.ResolveUDPAddr("udp", "localhost:8081")
+	logErr(err)
+
+	conn, err := net.ListenUDP("udp", addr)
+	logErr(err)
+
+	defer conn.Close()
+
+	fmt.Println("Servidor UDP aguardando requests...")
+
+	for {
+		handle(*conn)
+	}
+
+}
+
+func handle(conn net.UDPConn) {
+
+	req := make([]byte, 1024)
+	rep := make([]byte, 1024)
+
+	_, addr, err := conn.ReadFromUDP(req)
+	logErr(err)
+
+	fmt.Println(string(req[:]))
+
+	rep = []byte(strings.ToUpper(string(req)))
+
+	fmt.Println(string(rep[:]))
+
+	_, err = conn.WriteTo(rep, addr)
+	logErr(err)
 }
