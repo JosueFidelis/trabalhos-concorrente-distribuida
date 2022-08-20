0a8415e41e9e66e4bd57ac7e4454d32aab20f58e
Gabriel <gkqsj@cin.ufpe.br>
2022-08-20 20:58:56 -0300

    added working TCP interactions

--- exercicio_3/tcp/client/main.go
+++ exercicio_3/tcp/client/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"net"
-	"os"
 )
 
 func logErr(err error) {
@@ -18,18 +17,21 @@ func main() {
 	//connect to this socket
 	connClient, _ := net.Dial("tcp", "127.0.0.1:8080")
 
-	for {
+	text := "35,87,52,35,79,62,42,29,23,9,87,29,72,51,80,21,69,8,70,90\n"
+	//            10000
+	for i := 0; i < 2; i++ {
 
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Text to send: ")
-		text, err := reader.ReadString('\n')
-		logErr(err)
+		// reader := bufio.NewReader(os.Stdin)
+		// fmt.Print("Text to send: ")
+		// text, err := reader.ReadString('\n')
+		// logErr(err)
 
 		//send to socket
-		fmt.Fprint(connClient, text+"\n")
+		fmt.Println(text)
+		fmt.Fprint(connClient, text)
 
 		//listen for reply
-		//message, _ := bufio.NewReader(connClient).ReadString('\n')
-		//fmt.Print("Message from server: " + message)
+		message, _ := bufio.NewReader(connClient).ReadString('\n')
+		fmt.Print("Message from server: " + message)
 	}
 }
--- exercicio_3/tcp/server/main.go
+++ exercicio_3/tcp/server/main.go
@@ -4,6 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func logErr(err error) {
@@ -15,7 +18,7 @@ func logErr(err error) {
 
 func main() {
 	dstream, err := net.Listen("tcp", ":8080")
-	defer dstream.Close()
+	// defer dstream.Close()
 
 	logErr(err)
 
@@ -27,11 +30,34 @@ func main() {
 	}
 }
 
+func sortData(data string) string {
+
+	slc := strings.Split(data, ",")
+
+	var parsedSlc = []int{}
+
+	//parse to int
+	for _, i := range slc {
+		j, err := strconv.Atoi(i)
+		logErr(err)
+		parsedSlc = append(parsedSlc, j)
+	}
+	sort.Ints(parsedSlc)
+
+	return strings.Trim(strings.Join(strings.Fields(fmt.Sprint(parsedSlc)), " "), "[]")
+}
+
 func handle(con net.Conn) {
-	for {
+	for i := 0; i < 2; i++ {
 		data, err := bufio.NewReader(con).ReadString('\n')
 
 		logErr(err)
-		fmt.Println(data)
+		fmt.Println("Got:       ", data)
+
+		data = sortData(strings.TrimSpace(data))
+
+		_, err = con.Write([]byte(data + "\n"))
+		logErr(err)
 	}
+	con.Close()
 }
