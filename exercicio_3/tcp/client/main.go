beab2aa17735b5cb1070e00734ead24880825924
Gabriel <gkqsj@cin.ufpe.br>
2022-08-19 23:02:07 -0300

    Added server and client interactions

--- exercicio_3/tcp/client/main.go
+++ exercicio_3/tcp/client/main.go
@@ -1,5 +1,35 @@
-package client
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func logErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
 
 func main() {
+	//connect to this socket
+	connClient, _ := net.Dial("tcp", "127.0.0.1:8080")
 
+	for {
+
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Text to send: ")
+		text, err := reader.ReadString('\n')
+		logErr(err)
+
+		//send to socket
+		fmt.Fprint(connClient, text+"\n")
+
+		//listen for reply
+		//message, _ := bufio.NewReader(connClient).ReadString('\n')
+		//fmt.Print("Message from server: " + message)
+	}
 }
--- exercicio_3/tcp/server/main.go
+++ exercicio_3/tcp/server/main.go
@@ -1,5 +1,37 @@
-package server
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func logErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
 
 func main() {
+	dstream, err := net.Listen("tcp", ":8080")
+	defer dstream.Close()
 
+	logErr(err)
+
+	for {
+		con, err := dstream.Accept()
+		logErr(err)
+
+		go handle(con)
+	}
+}
+
+func handle(con net.Conn) {
+	for {
+		data, err := bufio.NewReader(con).ReadString('\n')
+
+		logErr(err)
+		fmt.Println(data)
+	}
 }
