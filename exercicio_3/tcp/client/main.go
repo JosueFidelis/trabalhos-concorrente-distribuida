8578b053a97a7397a3ee71eb550e4e16e70156df
JosueFidelis <jfon@cin.ufpe.br>
2022-08-21 02:34:04 -0300

    first version of rtt calculus on TCP

--- exercicio_3/tcp/client/main.go
+++ exercicio_3/tcp/client/main.go
@@ -3,12 +3,36 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
+	"math"
 	"net"
+	"os"
+	"strconv"
 	"strings"
+	"time"
 )
+
+func logDuration(fileName string) {
+
+	f, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+	log.Printf("%d\n", 1)
+}
 
 func main() {
 	//connect to this socket
+	numberOfClientsRunning, _ := strconv.Atoi(os.Args[1])
+	numberOfIterations := 10000
+	numberOfIterationsToDiscard := 1000
+
+	var rttArray = make([]float64, numberOfIterations)
+	rttMean := 0.0
+
 	connClient, _ := net.Dial("tcp", "127.0.0.1:8080")
 
 	//listen for reply
@@ -18,27 +42,42 @@ func main() {
 	connClient.Close()
 
 	newConnClient, _ := net.Dial("tcp", "127.0.0.1:"+strings.TrimSpace(newPort))
+	defer newConnClient.Close()
 
 	text := "35,87,52,35,79,62,42,29,23,9,87,29,72,51,80,21,69,8,70,90\n"
-	//            10000
-	for i := 0; i < 10000; i++ {
 
-		// reader := bufio.NewReader(os.Stdin)
-		// fmt.Print("Text to send: ")
-		// text, err := reader.ReadString('\n')
-		// logErr(err)
+	for i := 0; i < numberOfIterations+numberOfIterationsToDiscard; i++ {
 
 		//send to socket
 		// fmt.Println(text)
+		start := time.Now()
 		fmt.Fprint(newConnClient, text)
 
 		//listen for reply
-		message, _ := bufio.NewReader(newConnClient).ReadString('\n')
-		if i == 10000-1 {
-			fmt.Print("Message from server: " + message)
+		bufio.NewReader(newConnClient).ReadString('\n')
+		elapsed := time.Since(start).Nanoseconds()
+
+		if i-numberOfIterationsToDiscard >= 0 {
+			rttArray[i-numberOfIterationsToDiscard] = float64(elapsed)
+			rttMean += float64(elapsed)
 		}
-		//fmt.Print("Message from server: " + message)
 	}
 
-	newConnClient.Close()
+	if numberOfClientsRunning != -1 {
+		rttMean /= float64(numberOfIterations)
+		rttSd := 0.0
+
+		for i := 0; i < numberOfIterations; i++ {
+			rttSd += math.Pow(rttArray[i]-rttMean, 2)
+			fmt.Print(rttArray[i])
+			fmt.Print("  ;  ")
+		}
+		fmt.Print("\n")
+
+		rttSd = math.Sqrt(rttSd / float64(numberOfIterations))
+
+		fmt.Println(rttMean)
+		fmt.Println(rttSd)
+	}
+
 }
