3123d242828d1412a608edaaa731c0b7ee223019
JosueFidelis <jfon@cin.ufpe.br>
2022-10-02 16:32:10 -0300

    fix bug in run_pub_sub.bat

--- exercicio_6/run_pub_sub.bat
+++ exercicio_6/run_pub_sub.bat
@@ -1,11 +1,11 @@
 set /p messageSize="Choose message size"
 
-start cmd /k "go run .\publisher\publisher.go"
+start cmd /k "go run .\subscriber\subscriber.go %messageSize%"
 
 timeout /t 2 /nobreak
 
 for /l %%x in (1, 1, 40) do (
-   start cmd /k "go run .\subscriber\subscriber.go %messageSize%"
+   start cmd /k "go run .\publisher\publisher.go %messageSize%"
 )
 
-start cmd /k "go run .\client\main.go %messageSize% 1"
+start cmd /k "go run .\publisher\publisher.go %messageSize% 1"
